917db2184fa92c29fb9b5d5869e2f1c7e31e41c9
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-07-08 08:45:40 -0500

    Se implmenta la vista para los logs

---
+++ ViewModels/LogsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Repaso3.Services;
+using System.Collections.ObjectModel;
+
+namespace PinedaLEvaluacion3P.ViewModels;
+
+public partial class LogsViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<string> logs;
+
+    public LogsViewModel()
+    {
+        Logs = new ObservableCollection<string>();
+        CargarLogs();
+    }
+
+    private async void CargarLogs()
+    {
+        var registros = await LogService.LeerLogsAsync();
+        Logs.Clear();
+        foreach (var log in registros)
+        {
+            Logs.Add(log);
+        }
+    }
+}
