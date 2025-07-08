0f442b61f559122557af2549920314f2f0ae1a14
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-07-08 08:16:50 -0500

    Inservicion del servicio SQLite

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace PinedaLEvaluacion3P
+﻿using PinedaLEvaluacion3P.Services;
+namespace PinedaLEvaluacion3P
 {
     public partial class App : Application
     {
+
+        //Iniciacion de singleton
+        private static ProyectoService _database;
+
+        public static ProyectoService Database
+        {
+            get
+            {
+                //Si el _database es null crea una nueva instancia
+                if (_database == null)
+                {
+                    string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "proyectos.db3");
+                    _database = new ProyectoService(dbPath);
+
+                }
+                return _database;
+            }
+        }
+
+
         public App()
         {
             InitializeComponent();
---
+++ Services/ProyectoService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PinedaLEvaluacion3P.Models;
+using SQLite;
+
+namespace PinedaLEvaluacion3P.Services
+{
+    public class ProyectoService
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        public ProyectoService(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+
+            _db.CreateTableAsync<Proyecto>().Wait();
+        }
+
+
+        //Metodo publico apra insertar proyecto en la vista
+        public Task<int> InsertarProyectoAsync(Proyecto proyecto)
+        {
+            return _db.InsertAsync(proyecto);
+        }
+
+        //Leer los registros de la tabla Proyecto,
+        public Task<List<Proyecto>> ObtenerProyectoAsync()
+        {
+            return _db.Table<Proyecto>().ToListAsync();
+        }
+    }
+}
