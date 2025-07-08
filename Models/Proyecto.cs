19aee849983b6c27213a9e87308450fac6c24888
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-07-08 07:30:27 -0500

    Creacion del modelo para el registro de proyecto

---
+++ Models/Proyecto.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinedaLEvaluacion3P.Models
+{
+    public class Proyecto
+    {
+        //Creacion del modelo de registro de proyecto
+        [PrimaryKey, AutoIncrement]
+        private int Id { get; set; }
+
+        private string NombreProyecto { get; set; }
+
+        private string Responsable { get; set; }
+        private decimal Progreso { get; set; }
+        private int DuracionDias { get; set; }
+
+
+
+
+    }
+}
