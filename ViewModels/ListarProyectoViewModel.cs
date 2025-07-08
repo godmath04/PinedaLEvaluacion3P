3e3998b3cc52a348c9f35c45f19707f4597ae067
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-07-08 08:56:20 -0500

    Se agrego consol log para verificar error de que no aparecia el proyecto

--- ViewModels/ListarProyectoViewModel.cs
+++ ViewModels/ListarProyectoViewModel.cs
@@ -24,11 +24,28 @@ public partial class ListarProyectoViewModel : ObservableObject
 
     private async Task CargarProyectos()
     {
-        var lista = await App.Database.ObtenerProyectoAsync();
-        proyectos.Clear();
-        foreach (var proyecto in lista)
+        try
         {
-            proyectos.Add(proyecto);
+            var lista = await App.Database.ObtenerProyectoAsync();
+
+            if (lista == null)
+            {
+                Console.WriteLine("La lista de proyectos es NULL");
+                return;
+            }
+
+            Console.WriteLine($"Se encontraron {lista.Count} proyectos");
+
+            Proyectos.Clear();
+            foreach (var proyecto in lista)
+            {
+                Proyectos.Add(proyecto);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error cargando proyectos: " + ex.Message);
         }
     }
+
 }
